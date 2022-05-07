118a1ca56a90aa68b5ea1160770d253b220f2754
VuHuyHoangBC29 <demigodkiller912@gmail.com>
2022-05-07 21:32:26 +0700

    update

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ function getEle(id) {
     return document.getElementById(id);
 }
 
-function xetSoNguyenTo(i) {
+function xetSoNguyenTo(m) {
     var count = 0;
-    for (j = 1; j <= i; j++) {
-        if (i % j === 0) {
+    for (n = 1; n <= m; n++) {
+        if (m % n === 0) {
             count += 1;
         } else {
             count += 0;
@@ -22,7 +22,7 @@ getEle("btnBai5").onclick = function () {
     var result = "";
     for (i = 1; i <= n; i++) {
         var soNguyenTo = xetSoNguyenTo(i);
-        if (soNguyenTo <= 2) {
+        if (i !== 1 && i > 0 && soNguyenTo <= 2) {
             result += i + ' ';
         } else if (soNguyenTo > 2) {
             result += ''; 
